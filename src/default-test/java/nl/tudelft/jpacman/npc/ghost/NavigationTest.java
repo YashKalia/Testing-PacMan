0fcd8ec7e29d2dfdfaaf9ceea95c4e3558410422
Mauricio Aniche <mauricioaniche@gmail.com>
2019-04-17 16:49:32 +0200

    onem ore checkstyle rule

--- src/default-test/java/nl/tudelft/jpacman/npc/ghost/NavigationTest.java
+++ src/default-test/java/nl/tudelft/jpacman/npc/ghost/NavigationTest.java
@@ -1,7 +1,11 @@
 package nl.tudelft.jpacman.npc.ghost;
 
 import com.google.common.collect.Lists;
-import nl.tudelft.jpacman.board.*;
+import nl.tudelft.jpacman.board.Board;
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.board.Square;
+import nl.tudelft.jpacman.board.Unit;
 import nl.tudelft.jpacman.level.LevelFactory;
 import nl.tudelft.jpacman.level.MapParser;
 import nl.tudelft.jpacman.level.Pellet;
