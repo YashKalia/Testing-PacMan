69e70435a5a803f07dcba4e7cd5b4be99b1b1e99
bryan <B.Y.He@student.tudelft.nl>
2019-05-08 17:12:11 +0000

    a1e10 - answer to exercise 10

---
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -0,0 +1,115 @@
+package nl.tudelft.jpacman.npc.ghost;
+
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.level.*;
+import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.points.DefaultPointCalculator;
+import nl.tudelft.jpacman.points.PointCalculator;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class ClydeTest {
+    GhostMapParser ghostMapParser;
+    Player player;
+    List<String> text;
+
+    LevelFactory levelFactory;
+    GhostFactory ghostFactory;
+    BoardFactory boardFactory;
+
+    @BeforeEach
+    void SetUp(){
+        boardFactory=new BoardFactory(new PacManSprites());
+        ghostFactory=new GhostFactory(new PacManSprites());
+        levelFactory = new LevelFactory(new PacManSprites(), new GhostFactory(new PacManSprites()), new DefaultPointCalculator());
+        ghostMapParser=new GhostMapParser(levelFactory,boardFactory,ghostFactory);
+        PlayerFactory playerFactory = new PlayerFactory(new PacManSprites());
+        player = playerFactory.createPacMan();
+        text=new ArrayList<String>();
+    }
+
+    /*
+    Tests if Clyde moves toward PacMan when the distance is more than 8.
+     */
+    @Test
+    void farawaygoodweatherTest(){
+        text.add("##############");
+        text.add("#P.........C #");
+        text.add("##############");
+        Level level=ghostMapParser.parseMap(text);
+        System.out.println(level.getBoard().getHeight());
+        System.out.println(level.getBoard().getWidth());
+        player.setDirection(Direction.EAST);
+        level.registerPlayer(player);
+        System.out.println(player.getSquare());
+        Clyde thisisclyde=Navigation.findUnitInBoard(Clyde.class,level.getBoard());
+        assertEquals( Optional.of(Direction.WEST),thisisclyde.nextAiMove());
+
+    }
+    //###p c###
+
+    /*
+    Tests if Clyde doesn't moves away from PacMan when the distance is more than 8.
+     */
+    @Test
+    void farawaybadweatherTest() {
+        text.add("#P          C#");
+        Level level=ghostMapParser.parseMap(text);
+
+        level.registerPlayer(player);
+        Clyde thisisclyde=Navigation.findUnitInBoard(Clyde.class,level.getBoard());
+        assertNotEquals(thisisclyde.nextAiMove(),Direction.EAST);
+
+    }
+
+
+    /*
+    Tests if Clyde runs away from PacMan when the distance is less than 8.
+     */
+    @Test
+    void closebygoodweatherTest() {
+        text.add("#########");
+        text.add("#       #");
+        text.add("#C      #");
+        text.add("#       #");
+        text.add("#P      #");
+        text.add("#########");
+
+        player.setDirection(Direction.NORTH);
+        Level level=ghostMapParser.parseMap(text);
+        level.registerPlayer(player);
+
+        Clyde thisisclyde=Navigation.findUnitInBoard(Clyde.class,level.getBoard());
+        System.out.println(Navigation.findNearest(Player.class, thisisclyde.getSquare()));
+        assertEquals(thisisclyde.nextAiMove(),Optional.of(Direction.NORTH));
+
+    }
+
+    /*
+     Tests if Clyde doesn't move toward PacMan when the distance is less than 8.
+      */
+    @Test
+    void closebybadweatherTest() {
+        text.add("#P    C#");
+        Level level=ghostMapParser.parseMap(text);
+
+        level.registerPlayer(player);
+        Clyde thisisclyde=Navigation.findUnitInBoard(Clyde.class,level.getBoard());
+        System.out.println(Navigation.findNearest(Player.class, thisisclyde.getSquare()));
+        System.out.println(thisisclyde.nextAiMove());
+        assertNotEquals(Optional.of(Direction.WEST),thisisclyde.nextAiMove());
+
+    }
+
+
+
+}
