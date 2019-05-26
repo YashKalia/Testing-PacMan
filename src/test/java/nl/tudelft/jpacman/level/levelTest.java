fc4b97e9d9b008f5d895bfb11eb506654a482f51
Yash Kalia <yashkalia2@gmail.com>
2019-05-26 16:00:12 +0200

    e2

---
+++ src/test/java/nl/tudelft/jpacman/level/levelTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.jpacman.level;
+import static org.mockito.Mockito.*;
+
+
+import nl.tudelft.jpacman.board.BoardFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class levelTest {
+    LevelFactory levelFactory;
+    BoardFactory boardFactory;
+
+
+}
